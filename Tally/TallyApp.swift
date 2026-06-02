a9a44274bad9eed9c40534977d5457a671dc0852
Raghav Chalageri <raghavc2004@gmail.com>
2026-06-02 19:58:10 -0400

    Initial Commit

---
+++ Tally/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Tally
+//
+//  Created by Raghav Chalageri on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct TallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TallyTests/TallyTests.swift
@@ -0,0 +1,19 @@
+//
+//  TallyTests.swift
+//  TallyTests
+//
+//  Created by Raghav Chalageri on 6/2/26.
+//
+
+import Testing
+@testable import Tally
+
+struct TallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
